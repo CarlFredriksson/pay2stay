219cfdafe11b03babd1eb69e0d07dd70c5f88a19
Carl Fredriksson <c@msp.se>
2017-11-22 16:31:15 +0100

    Worked on runGame

---
+++ runGame.m
@@ -0,0 +1,44 @@
+% Every row in strategies is for 1 player.
+% Players receive 1 point per survived round.
+% 0 rows in alivePlayers denote dead players.
+function payouts=runGame(strategies)
+    numPlayers = size(strategies, 1);
+    maxNumRounds = size(strategies, 2);
+    payouts = zeros(numPlayers, 1);
+    alivePlayers = 1:numPlayers;
+    
+    for i=1:maxNumRounds
+        % Find lowest bidders
+        currentStrategies = strategies(:, i);
+        minVal = inf;
+        minIndices = [];
+        for j=1:length(alivePlayers)
+            currentPlayer = alivePlayers(j);
+            if (currentPlayer ~= 0)
+                currentVal = currentStrategies(currentPlayer);
+                if (currentVal < minVal)
+                    minVal = currentVal;
+                    minIndices = currentPlayer;
+                elseif (currentVal == minVal)
+                    minIndices = [minIndices; currentPlayer];
+                end
+            end
+        end
+        
+        % Remove lowest bidders
+        alivePlayers(minIndices) = 0;
+
+        % Break if no players left
+        if (sum(alivePlayers) == 0)
+            break;
+        end
+        
+        % Add 1 point for each surviving player
+        for j=1:length(alivePlayers)
+            currentPlayer = alivePlayers(j);
+            if (currentPlayer ~= 0)
+                payouts(currentPlayer) = payouts(currentPlayer) + 1;
+            end
+        end
+    end
+end
